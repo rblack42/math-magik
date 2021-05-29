2da69559f3c62fc463e5fbe3a6e6de63536a12a8
Roie Black <roie.black@gmail.com>
2021-05-29 10:33:33 -0500

    changed openscad version test to be >= 2019

--- tests/test_OpenSCAD.py
+++ tests/test_OpenSCAD.py
@@ -1,9 +1,8 @@
 from mmdesigner.OpenSCAD import OpenSCAD
-import os
 
 
 def test_OSC_version():
     mgr = OpenSCAD()
     version = mgr.get_version()
-    assert version == "2019.05"
-
+    year, rel = version.split(".")
+    assert int(year) >= 2019
