2cc16c0be7348397fedcdd4c8c1c3533307349ee
Roie Black <roie.black@gmail.com>
2021-05-29 20:48:31 -0500

    added mass properties code

--- mmdesigner/OpenSCAD.py
+++ mmdesigner/OpenSCAD.py
@@ -1,4 +1,8 @@
+import os
+import stl
+from stl import mesh
 import subprocess
+
 
 class OpenSCAD(object):
 
@@ -10,8 +14,106 @@ class OpenSCAD(object):
         version = result.stderr.decode().split()[2]
         return version
 
+    def gen_stl(self, scad_path):
+        err_code = 1
+        if not os.path.isfile(scad_path):
+            return err_code
+        if scad_path.endswith(".scad"):
+            base, ext = scad_path.split(".")
+            stl_path = base  + ".stl"
+            cmd = [
+                'openscad',
+                scad_path,
+                "-o",
+                stl_path
+            ]
+
+            result = subprocess.run(cmd, capture_output=True)
+            err_code = result.returncode
+            if not os.path.isfile(stl_path) or err_code != 0: # pragma: no cover
+                err_code = 1
+        return err_code
+
+    def _get_bounds(self):
+        minx = maxx = miny = maxy = minz = maxz = None
+        obj = self.mesh
+        for p in obj.points:
+            # p contains (x, y, z)
+            if minx is None:
+                minx = p[stl.Dimension.X]
+                maxx = p[stl.Dimension.X]
+                miny = p[stl.Dimension.Y]
+                maxy = p[stl.Dimension.Y]
+                minz = p[stl.Dimension.Z]
+                maxz = p[stl.Dimension.Z]
+            else:
+                maxx = max(p[stl.Dimension.X], maxx)
+                minx = min(p[stl.Dimension.X], minx)
+                maxy = max(p[stl.Dimension.Y], maxy)
+                miny = min(p[stl.Dimension.Y], miny)
+                maxz = max(p[stl.Dimension.Z], maxz)
+                minz = min(p[stl.Dimension.Z], minz)
+        self.minx = minx
+        self.maxx = maxx
+        self.miny = miny
+        self.maxy = maxy
+        self.minz = minz
+        self.maxz = maxz
+
+    def dump_to_json(self, json_path):
+        json = "{\n"
+        json += '  "maxx": "%s",\n' % self.maxx
+        json += '  "minx": "%s",\n' % self.minx
+        json += '  "maxy": "%s",\n' % self.maxy
+        json += '  "miny": "%s",\n' % self.miny
+        json += '  "maxz": "%s",\n' % self.maxz
+        json += '  "minz": "%s",\n' % self.minz
+        json += '  "volume": "%s",\n' % self.volume
+        json += '  "cgx": "%s",\n' % self.cgx
+        json += '  "cgy": "%s",\n' % self.cgy
+        json += '  "cgz": "%s",\n' % self.cgz
+        json += '  "ixx": "%s",\n' % self.ixx
+        json += '  "ixy": "%s",\n' % self.ixy
+        json += '  "ixz": "%s",\n' % self.ixz
+        json += '  "iyx": "%s",\n' % self.iyx
+        json += '  "iyy": "%s",\n' % self.iyy
+        json += '  "iyz": "%s",\n' % self.iyz
+        json += '  "izx": "%s",\n' % self.izx
+        json += '  "izy": "%s",\n' % self.izy
+        json += '  "izz": "%s"\n' % self.izz
+        json += '}\n'
+
+        with open(json_path,'w') as fout:
+            fout.write(json)
+
+
+    def get_properties(self, scad_file):
+        scad_path = os.path.abspath(scad_file)
+        err_code = self.gen_stl(scad_path)
+        base, ext = scad_path.split(".")
+        stl_file = base + ".stl"
+        self.json_file = base + ".json"
+        self.mesh = mesh.Mesh.from_file(stl_file)
+        self._get_bounds()
+        volume, cog, inertia = self.mesh.get_mass_properties()
+        self.cgx, self.cgy, self.cgz = cog
+        self.volume = volume
+        self.ixx, self.ixy, self.ixz = inertia[0]
+        self.iyx, self.iyy, self.iyz = inertia[1]
+        self.izx, self.izy, self.izz = inertia[2]
+        return err_code
+
+    def get_bounds(self):
+        return [
+                self.maxx, self.minx,
+                self.maxy, self.miny,
+                self.maxz, self.minz
+        ]
+
 if __name__ == '__main__':
     mgr = OpenSCAD()
-    mgr.get_version()
+    print(mgr.get_version())
+    mgr.get_properties("../tests/test_data/spar.scad")
+    mgr.dump_to_json("../tests/test_data/spar.json")
 
 
--- tests/test_OpenSCAD.py
+++ tests/test_OpenSCAD.py
@@ -1,3 +1,5 @@
+import os
+import json
 from mmdesigner.OpenSCAD import OpenSCAD
 
 
@@ -6,3 +8,44 @@ def test_OSC_version():
     version = mgr.get_version()
     year, rel = version.split(".")
     assert int(year) >= 2019
+
+
+def test_OSC_STL_generator():
+    mgr = OpenSCAD()
+    scad_file = "tests/test_data/spar.scad"
+    stl_file = "tests/test_data/spar.stl"
+    if os.path.exists(stl_file):
+        os.remove(stl_file)
+    mgr.gen_stl(scad_file)
+    assert os.path.isfile(stl_file)
+
+
+def test_OSC_STL_bad_file():
+    mgr = OpenSCAD()
+    assert mgr.gen_stl("bad") == 1
+
+
+def test_OSC_STL_bd_stl_name():
+    stl_file = "tests/test_data/spar.stl"
+    mgr = OpenSCAD()
+    assert mgr.gen_stl(stl_file) == 1
+
+
+def test_mass_properties():
+    mgr = OpenSCAD()
+    scad_file = "tests/test_data/spar.scad"
+    err_code = mgr.get_properties(scad_file)
+    assert err_code == 0
+    assert mgr.get_bounds() == [1.0, 0.0, 5.0, 0.0, 1.0, 0.0]
+
+
+def test_OSC_json():
+    mgr = OpenSCAD()
+    scad_file = "tests/test_data/spar.scad"
+    json_file = "tests/test_data/spar.json"
+    err_code = mgr.get_properties(scad_file)
+    assert err_code == 0
+    mgr.dump_to_json(json_file)
+    with open(json_file) as jin:
+        jdata = json.load(jin)
+        assert float(jdata["maxy"]) == 5.0
